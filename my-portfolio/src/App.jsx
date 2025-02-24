682d7d84eda8042abdb8872e9ca56ef24821c710
Emelie Nyberg <emelie.kedert@gmail.com>
2025-02-24 17:18:44 +0100

    fixed the image-bug

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -4,7 +4,7 @@ import { Bio } from "./Sections/Bio/Bio";
 import { Skills } from "./Sections/Skills/Skills";
 import { FeaturedProjects } from "./Sections/FeaturedProjects/FeaturedProjects";
 import { Tech } from "./Sections/Tech/Tech";
-import { Articles } from "./Sections/Articles/Articles";
+//import { Articles } from "./Sections/Articles/Articles";
 import { Contact } from "./Sections/Contact/Contact";
 import './App.css'
 
