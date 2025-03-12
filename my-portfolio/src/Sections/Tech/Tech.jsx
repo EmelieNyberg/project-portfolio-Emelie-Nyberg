b8acf6cb27233a180d992bf7b97d5af7c727b595
Emelie Nyberg <emelie.kedert@gmail.com>
2025-03-12 13:05:57 +0100

    added some more projects

--- my-portfolio/src/Sections/Tech/Tech.jsx
+++ my-portfolio/src/Sections/Tech/Tech.jsx
@@ -6,7 +6,7 @@ export const Tech = () => {
   return (
     <section className="tech-container">
       <h2>Tech</h2>
-      <h3>HTML, CSS, Flexbox, JavaScript, ES6, JSX, React, React Hooks, Node.js,  Mongo DB, Web Accessibility, APIs, mob-programming, pair-programming, GitHub. </h3>
+      <h3>HTML, CSS3, Flexbox, Grid, JavaScript, ES6, JSX, React, React Hooks, React router, TypeScript, Zustand, Styled Components, Responsive Design, Node.js, Express.js, Mongo DB (Compass & Atlas), Mongoose, RESTful APIs, Bcrypt, VS Code, Postman, Figma, Web Accessibility, APIs, mob-programming, pair-programming, GitHub. </h3>
     </section>
   );
 };
